0475f393a71e7099191ec08792aea620d54908c9
Timmy Lem <timmyxlem@gmail.com>
2023-04-04 16:05:06 -0700

    react router is correctly serving my pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,25 @@
 import * as React from 'react';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import * as ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import { render } from 'react-dom';
-import App from './App';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Bookmarks from './pages/Bookmarks';
+import ErrorPage from './pages/ErrorPage';
+// import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="bookmarks" element={<Bookmarks />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
