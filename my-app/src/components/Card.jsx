674d07448a25e4e51306863f1b7662c996b4de90
Timmy Lem <timmylem@Trams-MacBook-Pro.local>
2023-04-05 15:39:25 -0700

    added a modal and animations

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Modal from "./Modal.jsx";
 import styled from "styled-components";
 
 import "./Card.css";
 
 const Card = ({ result }) => {
   // console.log(result);
+  const [show, setShow] = useState(false);
+
   return (
     <div className="recipe-card">
       <h2>{result.strMeal}</h2>
       <img src={result.strMealThumb} alt={result.strMeal}></img>
       <p>Cuisine: {result.strArea}</p>
+      <button onClick={() => setShow(true)}>Show Recipe</button>
+      <Modal title="My Modal" onClose={() => setShow(false)} show={show}>
+        <p>This is modal body</p>
+      </Modal>
     </div>
   )
 }
--- my-app/src/components/SearchResult.jsx
+++ my-app/src/components/SearchResult.jsx
@@ -8,7 +8,6 @@ const SearchResult = ({ result }) => {
   return (
     <div
       className="search-result"
-      onClick={(e) => alert(`You clicked on ${result.strMeal}`)}
     >
       <Card result={result} />
     </div>
